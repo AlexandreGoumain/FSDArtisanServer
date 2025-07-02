d2774f83d742d8fc21610260c144203573fc61a3
Romain <romain.sales31@gmail.com>
2025-07-02 16:27:40 +0200

    fix(validation): fix validation for mongoose objectID

--- src/validations/furnitures.validation.ts
+++ src/validations/furnitures.validation.ts
@@ -1,44 +1,54 @@
 import { z } from "zod";
 import FurnitureCategory from "../models/furnitureCategories";
 import Ressource from "../models/ressources";
+import mongoose from "mongoose";
 
 export const createFurnitureValidation = z.object({
-    name: z.string().min(1, "Le nom est requis"),
-    description: z.string().nullable(),
-    quantity: z
-        .number()
-        .int()
-        .nonnegative("La quantité doit être un nombre entier positif"),
-    idCategory: z.string().refine(async (idCategory) => {
-        // Vérification en DB que l'id de la catégorie existe
-        const category = await FurnitureCategory.findById(idCategory);
-        if (!category) {
-            return false;
-        }
-        return true;
+  name: z.string().min(1, "Le nom est requis"),
+  description: z.string().nullable(),
+  quantity: z
+    .number()
+    .int()
+    .nonnegative("La quantité doit être un nombre entier positif"),
+  idCategory: z
+    .string()
+    .refine((id) => mongoose.Types.ObjectId.isValid(id), {
+      message: "L'id de la catégorie n'est pas un ObjectId valide",
+    })
+    .refine(async (idCategory) => {
+      // Vérification en DB que l'id de la catégorie existe
+      const category = await FurnitureCategory.findById(idCategory);
+      if (!category) {
+        return false;
+      }
+      return true;
     }, "L'id de la catégorie ne correspond à aucune catégorie existante"),
-    ressources: z
-        .array(
-            z.object({
-                idRessource: z.string().refine(async (idRessource) => {
-                    // Vérification en DB que l'id de la ressource existe
-                    const ressource = await Ressource.findById(idRessource);
-                    if (!ressource) {
-                        return false;
-                    }
-                    return true;
-                }, "L'id de la ressource ne correspond à aucune ressource existante"),
-                quantity: z
-                    .number()
-                    .int()
-                    .nonnegative(
-                        "La quantité de la ressource doit être un nombre entier positif"
-                    ),
-            })
-        )
-        .nonempty("La liste des ressources ne peut pas être vide"),
-    status: z.enum(["waiting", "in_production", "ready_to_sell"], {
-        message:
-            "L'état doit être 'waiting', 'in_production' ou 'ready_to_sell'",
-    }),
+  ressources: z
+    .array(
+      z.object({
+        idRessource: z
+          .string()
+          .refine((id) => mongoose.Types.ObjectId.isValid(id), {
+            message: "L'id de la ressource n'est pas un ObjectId valide",
+          })
+          .refine(async (idRessource) => {
+            // Vérification en DB que l'id de la ressource existe
+            const ressource = await Ressource.findById(idRessource);
+            if (!ressource) {
+              return false;
+            }
+            return true;
+          }, "L'id de la ressource ne correspond à aucune ressource existante"),
+        quantity: z
+          .number()
+          .int()
+          .nonnegative(
+            "La quantité de la ressource doit être un nombre entier positif"
+          ),
+      })
+    )
+    .nonempty("La liste des ressources ne peut pas être vide"),
+  status: z.enum(["waiting", "in_production", "ready_to_sell"], {
+    message: "L'état doit être 'waiting', 'in_production' ou 'ready_to_sell'",
+  }),
 });
--- src/validations/ressources.validation.ts
+++ src/validations/ressources.validation.ts
@@ -1,24 +1,35 @@
 import { z } from "zod";
 import RessourceCategory from "../models/ressourceCategories";
 import Supplier from "../models/suppliers";
+import mongoose from "mongoose";
 
 export const createRessourceValidation = z.object({
-    name: z.string().min(1, "Le nom de la ressource est requis"),
-    description: z.string().nullable(),
-    idCategory: z.string().refine(async (idCategory) => {
-        // Vérification en DB que l'id de la catégorie existe
-        const category = await RessourceCategory.findById(idCategory);
-        if (!category) {
-            return false;
-        }
-        return true;
+  name: z.string().min(1, "Le nom de la ressource est requis"),
+  description: z.string().nullable(),
+  idCategory: z
+    .string()
+    .refine((id) => mongoose.Types.ObjectId.isValid(id), {
+      message: "L'id de la catégorie n'est pas un ObjectId valide",
+    })
+    .refine(async (idCategory) => {
+      // Vérification en DB que l'id de la catégorie existe
+      const category = await RessourceCategory.findById(idCategory);
+      if (!category) {
+        return false;
+      }
+      return true;
     }, "L'id de la catégorie ne correspond à aucune catégorie existante"),
-    idSupplier: z.string().refine(async (idSupplier) => {
-        // Vérification en DB que l'id du fournisseur existe
-        const supplier = await Supplier.findById(idSupplier);
-        if (!supplier) {
-            return false;
-        }
-        return true;
+  idSupplier: z
+    .string()
+    .refine((id) => mongoose.Types.ObjectId.isValid(id), {
+      message: "L'id de la catégorie n'est pas un ObjectId valide",
+    })
+    .refine(async (idSupplier) => {
+      // Vérification en DB que l'id du fournisseur existe
+      const supplier = await Supplier.findById(idSupplier);
+      if (!supplier) {
+        return false;
+      }
+      return true;
     }, "L'id du fournisseur ne correspond à aucun fournisseur existant"),
 });
