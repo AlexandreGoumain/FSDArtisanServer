ebaf22d52d8e2413295d01a5a306492dc4a08f09
Romain <romain.sales31@gmail.com>
2025-07-02 11:54:38 +0200

    docs(supplier): add jsdoc to enrichSupplier()

--- src/utils/supplier.utils.ts
+++ src/utils/supplier.utils.ts
@@ -2,18 +2,23 @@ import Ressource from "../models/ressources";
 import { Schema } from "mongoose";
 import { ISupplier } from "../models/suppliers";
 
-
 export interface EnrichedSupplier extends ISupplier {
   ressourceCategories: Schema.Types.ObjectId[];
 }
 
-
-export const enrichSupplier = async (supplier: ISupplier): Promise<EnrichedSupplier> => {
+/**
+ * Enrichit un fournisseur avec la liste unique des catégories de ressources
+ * associées à ce fournisseur via le champ `idSupplier`.
+ *
+ * @param {ISupplier} supplier - Le fournisseur à enrichir.
+ * @returns {Promise<EnrichedSupplier>} Le fournisseur enrichi avec `ressourceCategories`.
+ */
+export const enrichSupplier = async (
+  supplier: ISupplier
+): Promise<EnrichedSupplier> => {
   const ressources = await Ressource.find({ idSupplier: supplier._id });
 
-  const ressourceCategories = [
-    ...new Set(ressources.map((r) => r.idCategory)),
-  ];
+  const ressourceCategories = [...new Set(ressources.map((r) => r.idCategory))];
 
   return {
     ...supplier.toObject(),
