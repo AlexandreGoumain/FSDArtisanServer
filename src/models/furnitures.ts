0dca1ef6197b582a8941e710f24fdf927f145978
Romain <romain.sales31@gmail.com>
2025-07-02 12:56:35 +0200

    fix(furniture): fix model properties

--- src/models/furnitures.ts
+++ src/models/furnitures.ts
@@ -2,81 +2,64 @@ import { Document, Schema, model } from "mongoose";
 
 // Interface TypeScript pour typer un utilisateur
 export interface IFurniture extends Document {
-    name: string;
-    idCategory: Schema.Types.ObjectId; // Référence à la catégorie
-    description: string;
-    ressources: Array<{
-        idRessource: Schema.Types.ObjectId; // Référence à la ressource
-        quantity: number; // Quantité de la ressource
-    }>;
-    quantity: number;
-    status: "waiting" | "in_production" | "ready_to_sell"; // État du meuble
+  name: string;
+  idCategory: Schema.Types.ObjectId; // Référence à la catégorie
+  description: string;
+  ressources: Array<{
+    idRessource: Schema.Types.ObjectId; // Référence à la ressource
+    quantity: number; // Quantité de la ressource
+  }>;
+  quantity: number;
+  status: "waiting" | "in_production" | "ready_to_sell"; // État du meuble
 }
 
 // Schéma Mongoose
-const furnitureSchema = new Schema<IFurniture>({
+const furnitureSchema = new Schema<IFurniture>(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     quantity: {
-        type: Number,
-        required: true,
-        min: 0, // Quantité ne peut pas être négative
+      type: Number,
+      required: true,
+      min: 0, // Quantité ne peut pas être négative
     },
     idCategory: {
-        type: Schema.Types.ObjectId,
-        ref: "Category", // Référence au modèle Category
-        required: true,
+      type: Schema.Types.ObjectId,
+      ref: "Category", // Référence au modèle Category
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["waiting", "in_production", "ready_to_sell"], // Énumération des états possibles
     },
     ressources: [
-        {
-            idRessource: {
-                type: Schema.Types.ObjectId,
-                ref: "Ressource", // Référence au modèle Ressource
-                required: true,
-            },
-            quantity: {
-                type: Number,
-                required: true,
-                min: 0, // Quantité ne peut pas être négative
-            },
-            idCategory: {
-                type: Schema.Types.ObjectId,
-                ref: "Category", // Référence au modèle Category
-                required: true,
-            },
-            ressources: [
-                {
-                    idRessource: {
-                        type: Schema.Types.ObjectId,
-                        ref: "Ressource", // Référence au modèle Ressource
-                        required: true,
-                    },
-                    quantity: {
-                        type: Number,
-                        required: true,
-                        min: 0, // Quantité de la ressource ne peut pas être négative
-                    },
-                },
-            ],
-            status: {
-                type: String,
-                required: true,
-                enum: ["waiting", "in_production", "ready_to_sell"], // Énumération des états possibles
-            },
+      {
+        idRessource: {
+          type: Schema.Types.ObjectId,
+          ref: "Ressource", // Référence au modèle Ressource
+          required: true,
         },
-        {
-            timestamps: true, // Ajoute createdAt et updatedAt
+        quantity: {
+          type: Number,
+          required: true,
+          min: 0, // Quantité ne peut pas être négative
         },
+      },
     ],
-});
+  },
+  {
+    timestamps: true, // Ajoute createdAt et updatedAt
+  }
+);
 
 // Modèle Mongoose
 const Furniture = model<IFurniture>("Furniture", furnitureSchema);
