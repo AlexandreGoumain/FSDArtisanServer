2b385fe1331408f5fddd9b2cfc2c3a6cd4b14ba1
Romain <romain.sales31@gmail.com>
2025-07-02 15:04:10 +0200

    fix(ressource): add error if delete ressource used in furniture

--- src/controllers/ressources.controller.ts
+++ src/controllers/ressources.controller.ts
@@ -1,134 +1,130 @@
 import { Request, Response } from "express";
 import { z } from "zod";
 import Ressource from "../models/ressources";
+import Furniture from "../models/furnitures";
 import standardResponse from "../utils/standardResponse";
 import { createRessourceValidation } from "../validations/ressources.validation";
 
 export const getAllRessources = async (req: Request, res: Response) => {
-    try {
-        const ressources = await Ressource.find();
-        standardResponse(
-            res,
-            200,
-            "Liste des ressources récupérée avec succès.",
-            ressources
-        );
-    } catch (error) {
-        standardResponse(
-            res,
-            500,
-            "Erreur serveur lors de la récupération des ressources.",
-            error
-        );
-    }
+  try {
+    const ressources = await Ressource.find();
+    standardResponse(
+      res,
+      200,
+      "Liste des ressources récupérée avec succès.",
+      ressources
+    );
+  } catch (error) {
+    standardResponse(
+      res,
+      500,
+      "Erreur serveur lors de la récupération des ressources.",
+      error
+    );
+  }
 };
 
 export const getRessourceById = async (req: Request, res: Response) => {
-    try {
-        const ressource = await Ressource.findById(req.params.id);
-        if (!ressource) {
-            standardResponse(res, 404, "Ressource non trouvée.");
-            return;
-        }
-        standardResponse(
-            res,
-            200,
-            "Ressource récupérée avec succès.",
-            ressource
-        );
-    } catch (error) {
-        standardResponse(
-            res,
-            500,
-            "Erreur serveur lors de la récupération de la ressource.",
-            error
-        );
+  try {
+    const ressource = await Ressource.findById(req.params.id);
+    if (!ressource) {
+      standardResponse(res, 404, "Ressource non trouvée.");
+      return;
     }
+    standardResponse(res, 200, "Ressource récupérée avec succès.", ressource);
+  } catch (error) {
+    standardResponse(
+      res,
+      500,
+      "Erreur serveur lors de la récupération de la ressource.",
+      error
+    );
+  }
 };
 
 export const createRessource = async (req: Request, res: Response) => {
-    try {
-        const { name, description, idCategory, idSupplier } =
-            await createRessourceValidation.parseAsync(req.body);
-        const newRessource = new Ressource({
-            name,
-            description,
-            idCategory,
-            idSupplier,
-        });
-        await newRessource.save();
-        standardResponse(
-            res,
-            201,
-            "Ressource créée avec succès.",
-            newRessource
-        );
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            standardResponse(res, 400, "Validation échouée.", error.errors);
-            return;
-        }
-        standardResponse(
-            res,
-            500,
-            "Erreur serveur lors de la création de la ressource.",
-            error
-        );
+  try {
+    const { name, description, idCategory, idSupplier } =
+      await createRessourceValidation.parseAsync(req.body);
+    const newRessource = new Ressource({
+      name,
+      description,
+      idCategory,
+      idSupplier,
+    });
+    await newRessource.save();
+    standardResponse(res, 201, "Ressource créée avec succès.", newRessource);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      standardResponse(res, 400, "Validation échouée.", error.errors);
+      return;
     }
+    standardResponse(
+      res,
+      500,
+      "Erreur serveur lors de la création de la ressource.",
+      error
+    );
+  }
 };
 
 export const updateRessource = async (req: Request, res: Response) => {
-    try {
-        const { name, description, idCategory, idSupplier } =
-            await createRessourceValidation.parseAsync(req.body);
-        const ressource = await Ressource.findByIdAndUpdate(
-            req.params.id,
-            { name, description, idCategory, idSupplier },
-            { new: true }
-        );
-        if (!ressource) {
-            standardResponse(res, 404, "Ressource non trouvée.");
-            return;
-        }
-        standardResponse(
-            res,
-            200,
-            "Ressource mise à jour avec succès.",
-            ressource
-        );
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            standardResponse(res, 400, "Validation échouée.", error.errors);
-            return;
-        }
-        standardResponse(
-            res,
-            500,
-            "Erreur serveur lors de la mise à jour de la ressource.",
-            error
-        );
+  try {
+    const { name, description, idCategory, idSupplier } =
+      await createRessourceValidation.parseAsync(req.body);
+    const ressource = await Ressource.findByIdAndUpdate(
+      req.params.id,
+      { name, description, idCategory, idSupplier },
+      { new: true }
+    );
+    if (!ressource) {
+      standardResponse(res, 404, "Ressource non trouvée.");
+      return;
     }
+    standardResponse(res, 200, "Ressource mise à jour avec succès.", ressource);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      standardResponse(res, 400, "Validation échouée.", error.errors);
+      return;
+    }
+    standardResponse(
+      res,
+      500,
+      "Erreur serveur lors de la mise à jour de la ressource.",
+      error
+    );
+  }
 };
 
 export const deleteRessource = async (req: Request, res: Response) => {
-    try {
-        const ressource = await Ressource.findByIdAndDelete(req.params.id);
-        if (!ressource) {
-            standardResponse(res, 404, "Ressource non trouvée.");
-            return;
-        }
-        standardResponse(
-            res,
-            200,
-            "Ressource supprimée avec succès.",
-            ressource
-        );
-    } catch (error) {
-        standardResponse(
-            res,
-            500,
-            "Erreur serveur lors de la suppression de la ressource.",
-            error
-        );
+  try {
+    const ressourceId = req.params.id;
+    const ressource = await Ressource.findByIdAndDelete(ressourceId);
+    if (!ressource) {
+      standardResponse(res, 404, "Ressource non trouvée.");
+      return;
     }
+
+    // Vérifie si la ressource est utilisée dans un meuble
+    const isUsedInFurniture = await Furniture.exists({
+      "ressources.idRessource": ressourceId,
+    });
+
+    if (isUsedInFurniture) {
+      return standardResponse(
+        res,
+        400,
+        "Impossible de supprimer cette ressource : elle est encore utilisée dans un ou plusieurs meubles."
+      );
+    }
+
+    standardResponse(res, 200, "Ressource supprimée avec succès.", ressource);
+  } catch (error) {
+    standardResponse(
+      res,
+      500,
+      "Erreur serveur lors de la suppression de la ressource.",
+      error
+    );
+  }
 };
